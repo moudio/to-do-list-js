204b45f120f0ee1fe6b22f9a5f9c848fc13bda53
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-12-24 15:10:59 +0000

    Removing the console.logs

--- src/functionalities/remove_project.js
+++ src/functionalities/remove_project.js
@@ -4,7 +4,6 @@ export default function removeProject(projectName){
     const projects = JSON.parse(localStorage.getItem('projects'));
     for(let i = 0 ; i < projects.length; i++){
         if(projects[i].name == projectName){
-            console.log(projectName)
             projects.splice(i, 1);
             if(projects.length === 0){
                 localStorage.removeItem('projects');
